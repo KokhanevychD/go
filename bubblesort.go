9ccc27df96cbc843a935266c9061a8bc6f87352a
Dmytro Kokhanevych <dmytro.kokhanevych@onix-systems.com>
2020-07-27 02:05:34 +0300

    fix bubblesort

--- bubblesort.go
+++ bubblesort.go
@@ -4,21 +4,26 @@ import (
 	"fmt"
 )
 
-func main(){
-	int_arr := []int{2, 212, 3001, 14, 501, 7800, 9932, 33, 45, 45, 45, 91, 99, 37, 102, 102, 104, 106, 109, 106}
-	res := bubblesort(int_arr)
+func main() {
+	intArr := []int{2, 212, 3001, 14, 501, 7800, 9932, 33, 45, 45, 45, 91, 99, 37, 102, 102, 104, 106, 109, 106}
+	res := Bubblesort(intArr)
 	fmt.Printf("%v", res)
 }
 
-func bubblesort(list []int) []int{
+// Bubblesort fun, sorting int arrays
+func Bubblesort(list []int) []int {
 	length := len(list)
-	for idx := range list{
-		if idx != length - 1{
-			if list[idx] > list[idx + 1]{
-				list[idx], list[idx + 1] = list[idx + 1], list[idx]
-				list = bubblesort(list)
+	sorted := true
+	for idx := range list {
+		if idx != length-1 {
+			if list[idx] > list[idx+1] {
+				list[idx], list[idx+1] = list[idx+1], list[idx]
+				sorted = false
 			}
 		}
+	}
+	if !sorted {
+		list = Bubblesort(list)
 	}
 	return list
 }
